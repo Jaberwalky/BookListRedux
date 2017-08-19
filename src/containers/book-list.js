f4d6de2391564c2c9ae719cf56498ba5aca9fc54
Paul Jablonski <pauljablonski@me.com>
2017-08-19 11:27:49 +0100

    Add initial reducer and connect method to book list

---
+++ src/containers/book-list.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class BookList extends Component {
+
+  renderList(){
+    return this.props.books.map( (book, index)=> {
+      return(
+        <li className="list-group-item" key={index}>{book.title}</li>
+      )
+    })
+  }
+
+  render(){
+    return(
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    books: state.books
+  }
+}
+
+export default connect(mapStateToProps)(BookList)
